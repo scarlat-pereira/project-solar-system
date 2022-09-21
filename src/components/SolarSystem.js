5a3a3703c08a31ca1758850bce7294fb5957eb6f
Scarlat Pereira <engcivil.scarlat@gmail.com>
2022-09-21 15:39:01 -0300

    cumprindo o quarto

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <h2>Solar System</h2>
+        <Title headline="Planetas" />
       </div>
     );
   }
