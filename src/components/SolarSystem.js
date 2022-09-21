a83b203d6f529515ca03ed574b51d282bf830a24
Scarlat Pereira <engcivil.scarlat@gmail.com>
2022-09-21 16:23:13 -0300

    arrumando o requisito nº 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Planets from '../data/planets';
 import PlanetCard from './PlanetCard';
-// import Title from './Title';
+import Title from './Title';
+
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        {/* <Title headline="Planetas" /> */}
+        <Title headline="Planetas" />
         <div>
           {Planets.map((planet) => (
             <PlanetCard
